9ba7f87aa72bcbd7ef97152e113ad2293995e7dd
korlend <xan-san@list.ru>
2018-03-24 21:34:22 +0400

    test

---
+++ src/main/java/com/korlend/chat/Application.java
@@ -0,0 +1,18 @@
+package com.korlend.chat;
+
+/**
+ * spizzheno by korlend on 13.12.2017.
+        */
+import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.ExecutionException;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ src/main/java/com/korlend/chat/servlets/UsersServlet.java
@@ -0,0 +1,23 @@
+package com.korlend.chat.servlets;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by korlend on 14.12.2017.
+ */
+@Controller
+@RequestMapping("/users")
+public class UsersServlet {
+
+    @RequestMapping("/getAll")
+    public List<String> getAllUsers() {
+        List<String> bla = new ArrayList<>();
+        bla.add("dimka");
+        bla.add("korlend");
+        return bla;
+    }
+}
